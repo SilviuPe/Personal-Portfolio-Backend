d39ac814260cb3bbc5feea65782cf7466baac131
SilviuPe <silviupetrea60@gmail.com>
2025-09-15 02:41:51 -0700

    fix: browser cors

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from database import Database
+from utils import gather_information_about_repo
 app = Flask(__name__)
+
+CORS(app)
 
 @app.route('/projects', methods=['GET'])
 def index():
@@ -16,8 +20,16 @@ def index():
             }, 400
 
         else:
+            data = gather_information_about_repo(projects[0]['github_link'])
 
-            return projects
+            if data:
+                return data, 200
+
+            else:
+                return {
+                    'status': 'error',
+                    'message': "Something went wrong."
+                }, 400
 
     except Exception as e:
         return {
