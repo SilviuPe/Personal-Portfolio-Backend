9b05cbb6be4221feac0c0b254a9cbca897cfa696
SilviuPe <silviupetrea60@gmail.com>
2025-09-15 20:47:51 -0700

    feat: added workflow image url to returned data

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def gather_information_about_repo(projects: list):
         repo_link = project['github_link']
 
         repo_data = {
-            'repo_link': repo_link,
+            'repo_url': repo_link,
         }
 
         link_elements = repo_link.split('/')
@@ -28,9 +28,14 @@ def gather_information_about_repo(projects: list):
 
         # Get the banner url
         banner_url = f'https://raw.githubusercontent.com/{username}/{repo_name}/{default_branch}/Banner.png'
-        i_response = requests.get(banner_url)
-        if i_response.status_code == 200:
+        workflow_url = f'https://raw.githubusercontent.com/{username}/{repo_name}/{default_branch}/Workflow.png'
+        banner_response = requests.get(banner_url)
+        workflow_response = requests.get(workflow_url)
+        if banner_response.status_code == 200:
             repo_data['banner_url'] = banner_url
+
+        if workflow_response.status_code == 200:
+            repo_data['workflow_url'] = workflow_url
 
         data.append(repo_data)
 
